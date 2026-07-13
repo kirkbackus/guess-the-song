abdcca2faa005a6db5f9534475544489b5126d43
Guerry <guerry@example.com>
2026-07-13 17:01:23 -0500

    Update app identifier to com.kirkbackus.guessthesong

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.guerry.guessthesong',
+  appId: 'com.kirkbackus.guessthesong',
   appName: 'Guess The Song',
   webDir: 'dist',
   server: {
